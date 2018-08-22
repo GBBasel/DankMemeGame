708a6533ccd77438b49390f2b55fc380db4e283a
Georg Schwan <georg.schwan@gbbasel.ch>
2018-08-22 15:45:27 +0200

    geo

--- actors/dino.py
+++ actors/dino.py
@@ -2,8 +2,17 @@ from gamegrid import *
 
 
 class Dino(Actor):
-    def __init__(self, path):
-        super().__init__(self, path)
+    def __init__(self):
+        Actor.__init__(self, "sprites/frog.gif")
+		print(1)
+		
+		print(self.getDirection())
+		self.inAir = False
+		self.speed = 10
 
     def act(self):
         self.move()
+
+    def onKeyRepeated(self, e):
+        if not self.inAir:
+			self.inAir = True
--- main.py
+++ main.py
@@ -2,9 +2,43 @@ import sys
 sys.path.append('H:\Desktop\DankMemeGame')
 from gamegrid import *
 
-from actors import Dino
+class Dino(Actor):
+    def __init__(self):
+        Actor.__init__(self, "sprites/frog.gif")
+        self.inAir = False
+        self.speed = 20
+        self.gravitation = -2
+        self.pressed = False
 
-makeGameGrid(800, 600, 1, None, "sprites/lane.gif", False)
+    def act(self):  
+        if self.inAir:
+            y = self.getY()
+            y += -self.speed
+            if self.pressed:
+                self.speed += 1
+            self.speed += self.gravitation
+            if y > 300:
+                self.setY(300)
+                self.inAir = False
+                self.speed = 20
+            else:
+                self.setY(y)
+        self.pressed = False
+
+    def onKeyRepeated(self, e):
+        self.inAir = True
+        self.pressed = True
+
+WIDTH = 800
+HEIGHT = 600
+speed = 10
+GROUND = HEIGHT // 2
+
+
+dino = Dino()
+makeGameGrid(WIDTH, HEIGHT, 1, None, "sprites/lane.gif", False, keyPressed=dino.onKeyRepeated)
 setSimulationPeriod(50)
+addActor(dino, Location(GROUND, GROUND), 90)
 show()
 doRun()
+
