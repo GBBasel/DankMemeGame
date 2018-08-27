a2e2bb125a872a12c5c102641595c97487472e91
Georg Schwan <georg.schwan@gbbasel.ch>
2018-08-27 15:26:33 +0200

    collision

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys
 sys.path.append('H:\Desktop\DankMemeGame')
 from gamegrid import *
+import random
 
 class Dino(Actor):
     def __init__(self):
         Actor.__init__(self, "sprites/frog.gif")
+        self.setCollisionRectangle(Point(0, 0), 50, 25)
         self.inAir = False
         self.speed = 30
         self.gravitation = -3
@@ -13,26 +15,35 @@ class Dino(Actor):
     def act(self):  
         if self.inAir:
             y = self.getY()
-            y += -self.speed
+            y -= self.speed
             self.speed += self.gravitation
-            if y > 300:
+            if self.pressed and self.speed > 0:
+                self.speed += 2
+            if y > GROUND:
                 self.reset()
             else:
                 self.setY(y)
+    
+
+    def collide(self, actor1, actor2):
+        print('ENDE')
+        return 0
                 
     def reset(self):
-        self.setY(300)
+        self.setY(GROUND)
         self.inAir = False
         self.speed = 20
 
-    def onKeyRepeated(self, e):
+    def onkeyPressed(self, e):
         self.inAir = True
-        if self.speed > 0:
-            self.speed += 1
+        self.pressed = True
+
+    def onKeyReleased(self, e):
+        self.pressed = False
             
 class Gegner(Actor):
-    def __init__(self):
-        Actor.__init__(self, "sprites/frog.gif")
+    def __init__(self, path='sprites/frog.gif'):
+        Actor.__init__(self, path)
         self.speed = 5
 
     def act(self):
@@ -47,23 +58,38 @@ class Gegner(Actor):
     def destroy(self):
         if self.getX() < -100:
             del self
+            
+class Kaktus(Gegner):
+    def __init__(self, path='sprites/frog.gif'):
+        Gegner.__init__(self, path)
+        self.speed = 10
+    
 
 WIDTH = 800
 HEIGHT = 600
-speed = 10
 GROUND = HEIGHT // 2
 
 
 dino = Dino()
-makeGameGrid(WIDTH, HEIGHT, 1, None, "sprites/lane.gif", False, keyPressed=dino.onKeyRepeated)
+makeGameGrid(WIDTH, HEIGHT, 1, None, "sprites/lane.gif", False, keyPressed=dino.onkeyPressed, keyReleased=dino.onKeyReleased)
 setSimulationPeriod(50)
 addActor(dino, Location(GROUND, GROUND), 90)
 show()
 doRun()
 
+gegner = 0
+
 while not isDisposed():
-    addActor(Gegner(), Location(WIDTH +100, GROUND), 90)
-    delay(1000)
+    if gegner > 100 and 0.005 > random.random():
+        kaktus = Kaktus()
+        addActor(kaktus, Location(WIDTH +100, GROUND), 90)
+        dino.addCollisionActor(kaktus)
+        gegner = 0
+    if 0.005 > random.random():
+        addActor(Gegner(), Location(WIDTH +100, random.randint(0, GROUND-50)), 90)
+    
+    gegner += 1
+    delay(10)
     
     
     
