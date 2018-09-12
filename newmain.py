c6ef1530857636e8df7330707827ce00212c46f1
TheTinGoesSkra <vincent.ellenrieder@stud.edubs.ch>
2018-09-12 15:26:22 +0200

    comments

--- newmain.py
+++ newmain.py
@@ -3,8 +3,10 @@ from soundsystem import *
 import random
 import cmath
 import math
+import time
 
-PATH_TO_FILE = 'H:\Desktop\\DankMemeGame\\'
+#Für jeden PC anders
+PATH_TO_FILE = 'C:\Users\\vince\\OneDrive\\Documents\\Bäumlihof\\5Bb\\Informatik\\DankMemeGame\\'
 
 class Dino(Actor):
     def __init__(self):
@@ -19,14 +21,18 @@ class Dino(Actor):
         self.gravitation = -3
         self.pressed = False
         self.score = 0
+        self.startTime = time.time()
 
     def act(self):
         if self.inAir:
+            #beschleunigung berechnung
             y = self.getY()
             y -= self.speed
             self.speed += self.gravitation
+            # wenn man eine taste drückt und der dino nicht fällt, fällt er langsamer 
             if self.pressed and self.speed > 0:
                 self.speed += 2
+                
             if y > GROUND:
                 self.putToGround()
             else:
@@ -39,18 +45,22 @@ class Dino(Actor):
         return 0
                 
     def putToGround(self):
+        #reset dino
         self.setY(GROUND)
         self.inAir = False
         self.doubleJump = False
         self.speed = 20
 
     def onkeyPressed(self, e):
+        #wird ausgefüht wenn eine taste gedrückt wird
+        #double jump
         if not self.doubleJump and self.inAir and not self.pressed:
             self.speed += self.boost
             self.doubleJump = True
         
         self.inAir = True
         self.pressed = True
+        #wenn das spiel vorbei ist wird es neu gestartet
         if self.gameOver:
             self.restart()
         
@@ -59,15 +69,24 @@ class Dino(Actor):
     
     def game_over(self):
         self.gameOver = True
-        self.gameOverText = TextActor("NOOB Game Over NOOB", Color.BLACK, Color.WHITE, Font("Arial", Font.BOLD, 60))
+        self.gameOverText = TextActor("Game Over Score: {}".format(self.score), Color.BLACK, Color.WHITE, Font("Arial", Font.BOLD, 60))
         addActor(self.gameOverText, Location(WIDTH//2-self.gameOverText.getWidth(0)//4, GROUND))
         doPause()
     
     def restart(self):
+        # global
         self.gameOver = False
         removeActor(self.gameOverText)
+        #dino
         self.putToGround()
-        for gegner in gegners:
+        self.score = 0
+        #boss
+        removeActor(boss)
+        self.startTime = time.time()
+        boss.aktive = False
+        boss.onespawned = False
+        #gegner
+        for gegner in gegnersliste:
             removeActor(gegner)
         doRun()
         
@@ -76,16 +95,18 @@ class Gegner(Actor):
     def __init__(self, path='sprites/frog.gif'):
         Actor.__init__(self, path)
         self.speed = 5
-        self.cdirection = 1+0j
+        self.cdirection = 1+0j # richtung mit einer komplexen zahl
         
     def act(self):
         self.move()
         self.destroy()
         
     def turn(self, winkel):
+        #dreht sich um 'winkel' in radian
         self.cdirection *= cmath.rect(1, winkel)
     
     def move(self):
+        #bewegung
          x = self.getX()
          y = self.getY()
          x -= int(self.speed * self.cdirection.real * SPEEDMULTIPLIERT)
@@ -94,8 +115,10 @@ class Gegner(Actor):
          self.setY(y)
          
     def destroy(self):
+        #löscht objekt
         if self.getX() < -100:
             removeActor(self)
+            del self
             
 class Kaktus(Gegner):
     def __init__(self, path=PATH_TO_FILE + 'bilder/obiwandank.png'):
@@ -105,11 +128,12 @@ class Kaktus(Gegner):
 class Vogel(Gegner):
     def __init__(self, path=PATH_TO_FILE + 'bilder/bird60.png'):
         Gegner.__init__(self, path)
-        self.speed = random.randint(10, 20)
+        self.speed = random.randint(10, 20) # zufällige geschwindigkeit
         self.turn(random.uniform(-0.2, 0.2))
     
         
     def act(self):
+        #ändert zufällig seine richtung
         if random.random() < 0.05:
             self.turn(random.uniform(-0.5, 0.5))
         self.move()
@@ -117,22 +141,32 @@ class Vogel(Gegner):
         
         
 class Boss(Gegner):
-    def __init__(self, path='sprites/car1.gif'):
+    def __init__(self, path='bilder/boss200.png'):
         Gegner.__init__(self, path)
-        self.alive = 0   
+        self.alive = 0  #variable für die bewegung
+        self.aktive = False # True wenn er da ist
+        self.onespawned = False # spawned nur einmal pro game
+        self.timeSpawned = None # zeitpunkt des spawnens
     
     def act(self):
+        # nach 10 sekunden wird er despawned
+        if time.time()-self.timeSpawned > 10:
+            self.aktive = False
+            removeActor(self)
+        # brechnet und setzt position
         self.alive += 0.1
-        print(math.sin(self.alive))
         y = int(300 + math.sin(self.alive)*100)
         self.setY(y)
-        if 0.05 > random.random():
+        #spawned bullets
+        if 0.025 > random.random():
             bullet = Bullet()
             addActor(bullet, Location(self.getX(), self.getY()), 90)
             dino.addCollisionActor(bullet)
+            gegnersliste.append(bullet)
+        
        
 class Bullet(Gegner):
-    def __init__(self, path='sprites/frog.gif'):
+    def __init__(self, path='bilder/bullet.png'):
         Gegner.__init__(self, path)
         self.speed = 20
         
@@ -141,28 +175,38 @@ WIDTH = 800
 HEIGHT = 600
 GROUND = HEIGHT // 2
 
-SPEEDMULTIPLIERT = 1
+SPEEDMULTIPLIERT = 1 # geschwindigkeit der gegenr
 
 
 dino = Dino()
+
 makeGameGrid(WIDTH, HEIGHT, 1, None, PATH_TO_FILE + 'bilder/backgroundneu800.png', False, keyPressed=dino.onkeyPressed, keyReleased=dino.onKeyReleased)
 setSimulationPeriod(50)
+
 addActor(dino, Location(GROUND, GROUND), 90)
 
 boss = Boss()
-addActor(boss, Location(GROUND+350, GROUND), 90)
+
 show()
 doRun()
 
 
-gegnerLauf = 0
-gegners = []
+gegnerLauf = 0 #verhindert zu viele gegner spawns
+gegnersliste = [] # speichert alle gegner um sie spàter zu löschen
 
 
-
+#Game Loop
 while not isDisposed():
-    if gegnerLauf > 80 and 0.05 > random.random():
-        if 0.5 > random.random():
+    # Boss Spawn
+    if time.time()-dino.startTime > 30 and not boss.aktive and not boss.onespawned: 
+        #init boss
+        boss.aktive = True
+        boss.onespawned = True
+        boss.timeSpawned = time.time()
+        addActor(boss, Location(GROUND+350, GROUND), 90)
+    # Gegner Spawn
+    if gegnerLauf > 80 and 0.05 > random.random() and not boss.aktive:
+        if 0.5 > random.random():  # 50%
             gegner = Kaktus()
             addActor(gegner, Location(WIDTH +100, GROUND), 90)
         else:
@@ -170,23 +214,16 @@ while not isDisposed():
             addActor(gegner, Location(WIDTH +100, random.randint(50, GROUND-50)), 90)
         dino.addCollisionActor(gegner)
         gegnerLauf = 0
-        gegners.append(gegner)
-    if 0.005 > random.random():
+        gegnersliste.append(gegner)
+    # Wolken Spawn
+    if 0.005 > random.random() and not dino.gameOver:
         addActor(Gegner(path=PATH_TO_FILE + 'bilder/cloud100.png'), Location(WIDTH +100, random.randint(50, GROUND-50)), 90)
         
     if dino.gameOver:
         SPEEDMULTIPLIERT = 1
-    else:
+    elif not boss.aktive:
         SPEEDMULTIPLIERT += 0.001
         
 
     gegnerLauf += 1
     delay(10)
-<<<<<<< Updated upstream
-        
-
-=======
-    
-    
-          
->>>>>>> Stashed changes
